b63bd5be90c6085d7e63933315f1a32d188b51c0
João Marcos <joaogardingo@clippingcacd.com.br>
2020-04-27 23:41:04 -0300

    testes aprovados, desafio concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const IncomeArray = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const OutcomeArray = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    function TypeReducer(acc: number, curr: Transaction): number {
+      return acc + curr.value;
+    }
+
+    const income = IncomeArray.reduce(TypeReducer, 0);
+    const outcome = OutcomeArray.reduce(TypeReducer, 0);
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ type, title, value });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
